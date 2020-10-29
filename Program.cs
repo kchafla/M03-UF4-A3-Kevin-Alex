eed16378d07c1e2b557b1d0463f560461c60c1d6
alex2001-daw <abalaguer@fp.insjoaquimmir.cat>
2020-10-29 20:18:22 +0100

    Ronda y Pajaros

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
 ﻿using System;
 
 class MainClass {
+  Pajaro rojo = new Pajaro("Red", "cardenal", "ninguno", 2);
+  Pajaro amarillo = new Pajaro("Chuck", "canario", "velocidad", 23);
+  Pajaro azul1 = new Pajaro("Jay", "azulejo", "dividirse", 64);
+  Pajaro azul2 = new Pajaro("Jake", "azulejo", "dividirse", 64);
+  Pajaro azul3 = new Pajaro("Jim", "azulejo", "dividirse", 64);
+  Pajaro verde = new Pajaro("Hal", "tucan", "bumerang", 45);
+  Pajaro blanco = new Pajaro("Bomb", "cuervo", "explotar", 67);
+  Pajaro negro = new Pajaro("Matilda", "gallina", "huevo explosivo", 91);
+  Pajaro naranja = new Pajaro("Bubbles", "gorrion", "hincharse", 13);
+  Pajaro rosa = new Pajaro("Stella", "cacatua Galah", "burbujas", 31);
+  Pajaro rojoGordo = new Pajaro("Terence", "cardenal", "gordo", 44);
+
   public static void Main () {
     Console.WriteLine ("Hello World");
   }
---
+++ Ronda.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Ronda{
+    
+    int numeroRonda;
+
+    static string CompararPajaros(Pajaro pj1, Pajaro pj2){
+        if (pj1.energia > pj2.energia){
+
+            return ;
+        }else if(pj1.energia < pj2.energia){
+
+        }else
+    }
+
+}
