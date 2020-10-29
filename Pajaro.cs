11da011bc7b2fc53b020553661bee17bad6530de
alex2001-daw <abalaguer@fp.insjoaquimmir.cat>
2020-10-29 19:28:11 +0100

    Pajaro

---
+++ Pajaro.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Pajaro{
+    private string nombre;
+    private string especie;
+    private string poder;
+    private int energia;
+
+    static void CrearPajaro(string newNombre, string newEspecie, string newPoder, int newEnergia){
+        nombre = newNombre;
+        especie = newEspecie;
+        poder = newPoder;
+        energia = newEnergia;
+    }
+}
