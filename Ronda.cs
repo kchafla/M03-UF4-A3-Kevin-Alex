a21c988d8036f573e8ddcb6ff4a6c223eae57e4a
kchafla <kchaflam@fp.insjoaquimmir.cat>
2020-11-03 19:56:11 +0100

    Comentarios

--- Ronda.cs
+++ Ronda.cs
@@ -1,6 +1,6 @@
 public class Ronda{
     
-    //Comparamos los pajaros para ver quien ha ganado
+    //Comparamos los pajaros para ver quien ha ganado.
     public int CompararPajaros(int pj1, int pj2) {
         if (pj1 > pj2) {
             return 1;             
